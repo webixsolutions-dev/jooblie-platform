7295d10782daf4c667bdb363eecdb81950b372cc
hashhaam <hashamulhaq068@gmail.com>
2026-07-23 22:46:48 +0500

    core: add application, saved-job, and notification hooks

--- packages/core/src/queries/index.ts
+++ packages/core/src/queries/index.ts
@@ -1,4 +1,11 @@
 export {
+  useApplication,
+  useApply,
+  useMyApplications,
+  type ApplicationDetail,
+  type ApplyInput,
+} from "./applications";
+export {
   useJob,
   useJobs,
   type JobDetailRow,
@@ -6,6 +13,19 @@ export {
   type JobsPage,
   type UseJobsParams,
 } from "./jobs";
+export {
+  useMarkNotificationRead,
+  useNotifications,
+  useUnreadCount,
+  type MarkNotificationReadInput,
+  type NotificationListRow,
+} from "./notifications";
 export * from "./query-client";
 export * from "./query-keys";
+export {
+  useSavedJobs,
+  useToggleSaveJob,
+  type SavedJobListRow,
+  type ToggleSaveJobInput,
+} from "./saved-jobs";
 export { useCategories, useSectors } from "./taxonomy";
