c6f52070eb88afa32f240b8fe6fef1068790775a
hashhaam <hashamulhaq068@gmail.com>
2026-07-23 22:07:26 +0500

    chore: enforce Supabase client import boundary

--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -1,1 +1,11 @@
-export { default } from "@jooblie/config/eslint";
+import sharedConfig from "@jooblie/config/eslint";
+
+export default [
+  ...sharedConfig,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    rules: {
+      "no-restricted-imports": "off",
+    },
+  },
+];
