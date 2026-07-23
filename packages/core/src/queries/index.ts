35aeb0905b315fde686423c22df987f19adf5cd8
hashhaam <hashamulhaq068@gmail.com>
2026-07-23 22:46:27 +0500

    core: add job listing, detail, and taxonomy hooks

---
+++ packages/core/src/queries/index.ts
@@ -0,0 +1,11 @@
+export {
+  useJob,
+  useJobs,
+  type JobDetailRow,
+  type JobListRow,
+  type JobsPage,
+  type UseJobsParams,
+} from "./jobs";
+export * from "./query-client";
+export * from "./query-keys";
+export { useCategories, useSectors } from "./taxonomy";
